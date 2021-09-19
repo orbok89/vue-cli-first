bf835f448362ce7165c4723ff420ed6ee7ea83fb
BigFelk <fedferrara89@gmail.com>
2021-09-19 13:39:14 +0200

    prova push

---
+++ src/components/titolo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        la mia prima componente di vue cli
+    </div>
+</template>
+<script>
+
+
+export default {
+  name: 'titolo',
+  props: {
+    msg: String
+  }
+}
+</script>
