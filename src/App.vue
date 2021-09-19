1b4b0973f573a9f886957b9f0d16ef1a86308a07
BigFelk <fedferrara89@gmail.com>
2021-09-19 13:41:46 +0200

    prova due

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <titolo/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import titolo from './components/titolo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    titolo
   }
 }
 </script>
